b188aeeac080c6792061613333a718f3cd64d139
Rez-Laptop <rezelute@gmail.com>
2020-02-04 07:34:23 +0000

    .

--- schema.js
+++ schema.js
@@ -1,9 +1,38 @@
-const GraphqlObjectType = require("graphql");
+const WarriorData = require ("./fake-data");
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLID
+} = require('graphql');
 
 //launch type
-const launchType = new GraphqlObjectType({
-  name: "Launch",
+const WarriorType = new GraphQLObjectType({
+  name: 'warrior',
   fields: () => ({
-    console.log('hello')
+    id: {type: GraphQLID},
+    fighting_style: {type: GraphQLString},
+    clan: {type: GraphQLString}
   })
-})
+});
+
+const rootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    Warrior: {
+      type: WarriorType,
+      args: {
+        id: { GraphQLID }
+      },
+      resolve(parent, args) {
+        return WarriorData.find(x=>x.id=== args.id);
+      }
+    }
+  }
+});
+
+console.log(rootQuery);
+
+module.exports = new GraphQLSchema({query: rootQuery});
--- server.js
+++ server.js
@@ -1,16 +1,52 @@
+// var express = require('express');
+// var graphqlHTTP = require('express-graphql');
+// var { buildSchema } = require('graphql');
+
+// // Construct a schema, using GraphQL schema language
+// var schema = buildSchema(`
+//   type Query {
+//     hello: String,
+//     world(pickMyNums: [Int]): [Int]!
+//   }
+// `);
+
+// // The root provides a resolver function for each API endpoint
+// var root = {
+//   hello: () =>
+//   {
+//     return 'Hello world!';
+//   },
+//   world: ({ pickMyNums }) => { //args is destructured
+//     let arrayer = [12, 3, 4, 5, 6];
+//     // console.log([12, 3, 4, 5, 6].find(x=>x===12));
+//     return (pickMyNums === undefined ? arrayer : arrayer.filter(function (item) {
+//       return pickMyNums.includes(item);
+//     }) )
+//   }
+// };
+
+// var app = express();
+// app.use('/graphql', graphqlHTTP({
+//   schema: schema,
+//   rootValue: root,
+//   graphiql: true,
+// }));
+// app.listen(4000);
+// console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+
 const express = require("express");
-const graphqlHttp = require("express-graphql");
+const expressGraphQL = require("express-graphql");
 const schema = require("./schema");
-
+// const graphql = require('graphql');
 const app = express();
-
 
 
 app.use(
   '/graphql',
-  graphqlHTTP({
+  expressGraphQL({
     schema: schema ,
-    graphiql: true,
+    graphiql: true
   }),
 );
 
